4c7d1cc9c1fe6f7e8b8c32b7f39d7c38b2e445de
ejoi8 <ejoi8@yahoo.com>
2020-02-09 01:01:28 +0800

    enhanced home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,22 @@
-@extends('layouts.app')
+@extends('adminlte::page')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+<div class="card">
+  <div class="card-header">
+    <h3 class="card-title">Dashboard</h3>
+    <div class="card-tools">
+      <!-- Buttons, labels, and many other things can be placed here! -->
+      <!-- Here is a label for example -->
+      <span class="badge badge-primary">Label</span>
     </div>
+    <!-- /.card-tools -->
+  </div>
+  <!-- /.card-header -->
+  <div class="card-body">
+    Welcome <strong>{{ Auth::user()->name }}</strong>
+  </div>
+  <!-- /.card-body -->
 </div>
+<!-- /.card -->
 @endsection
